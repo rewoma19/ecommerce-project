dc9475f9cfa18d6f8ff00d89cd8c290bc62cb278
Raphael <rewoma19@gmail.com>
2025-08-14 00:32:01 -0600

    Make new pages using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      {/* writing "index" is a shortcut for path="/" for the home page */}
+      <Route index element={<HomePage />} />
+      {/* <Route path="/" element={<HomePage />} /> */}
+      <Route path="checkout" element={<CheckoutPage />} />
+    </Routes>
   );
 }
 
